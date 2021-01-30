34a9d4b2b30e6bf2d2b96dc8e3be60f7ac3ef12b
Jon Skoberne <jon.skoberne@outfit7.com>
2021-01-30 16:35:06 +0100

    Add grid world

---
+++ Assets/Code/TileBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+    public TileTypes type;
+    public bool hasUnit;
+    public bool isRange;
+    public GameObject placedUnitRef;
+
+    private Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetSurfacePosition()
+    {
+        Vector3 tmp = this.transform.position;
+        return new Vector3(tmp.x,  tmp.y + renderer.bounds.size.y/2.0f, tmp.z);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (renderer != null)
+        {
+            var a = GetSurfacePosition();
+            Gizmos.DrawCube(a, Vector3.one/10.0f);
+        }
+    }
+}
---
+++ Assets/Code/TileTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTypes : MonoBehaviour
+{
+    public enum TileType
+    {
+        Forest,
+        BurntForest,
+        River,
+        DriedRiver,
+        Grassland,
+        Mountain,
+        Pitfall,
+        Magic
+    }
+}
