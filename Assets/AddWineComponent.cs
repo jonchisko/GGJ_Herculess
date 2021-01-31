2d42cf1cb3d2bae5c6f7383beb8cd4fabb87fb5f
DomenPozrl <domenpozrl1994@gmail.com>
2021-01-31 04:34:24 +0100

    HUD

---
+++ Assets/AddWineComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddWineComponent : MonoBehaviour
+{
+    public GameObject winePrefab;
+    public int worldX;
+    public int worldY;
+
+    public int worldZ;
+    public void spawnObject()
+    {
+        Vector3 p = Input.mousePosition;
+        Vector3 pos = Camera.main.ScreenToWorldPoint( p);
+        
+
+        GameObject a = Instantiate(winePrefab) as GameObject;
+        a.transform.position =  new Vector3(worldX, worldY, worldZ); //pos +
+    }
+}
---
+++ Assets/ThunderComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject thunderPrefab;
+    public int worldX;
+    public int worldY;
+
+    public int worldZ;
+    public void spawnObject()
+    {
+        Vector3 p = Input.mousePosition;
+        Vector3 pos = Camera.main.ScreenToWorldPoint( p);
+        
+
+        GameObject a = Instantiate(thunderPrefab) as GameObject;
+        a.transform.position =  new Vector3(worldX, worldY, worldZ); //pos +
+    }
+}
